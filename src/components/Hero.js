9841bb1e88f2a0d9c379c56c085bc182a1f002e9
TheekshR <theeksh99@gmail.com>
2025-12-14 00:32:49 +0530

    changes on hero js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -39,7 +39,9 @@ const Hero = () => {
         </div>
       </div>
       <div className="hero-right">
-        <Spline scene="https://prod.spline.design/AZCQW9zVN6xJaAE0/scene.splinecode " />
+        <div className="spline-wrapper">
+          <Spline scene="https://prod.spline.design/AZCQW9zVN6xJaAE0/scene.splinecode" />
+        </div>
       </div>
     </section>
   );
