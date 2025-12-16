3ed2b424f65b4fcfdbbc143ec2b24b72873dafe0
TheekshR <theeksh99@gmail.com>
2025-12-16 06:15:45 +0530

    mobile optimize the hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import '../styles/Hero.css';
 import Spline from '@splinetool/react-spline';
+import portfolioImg from '../assets/portfolio.png';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
@@ -33,6 +34,9 @@ const Hero = () => {
     <section ref={heroRef} className="hero" id="home">
       <div className="hero-left">
         <div className="hero-content">
+          {/* Static Image for Mobile Only */}
+          <img src={portfolioImg} alt="Theekshana" className="mobile-image" />
+
           <p className="hero1">Hey, It's me</p>
           <p className="hero2">Theekshana</p>
           <p className="hero3">Undergraduate BSc(Hons) in Information Technology | Specializing in Data Science</p>
@@ -41,8 +45,9 @@ const Hero = () => {
       <div className="hero-right">
         <div className="spline-wrapper">
           <Spline scene="https://prod.spline.design/AZCQW9zVN6xJaAE0/scene.splinecode" />
-        </div>
+        </div> 
       </div>
+      
     </section>
   );
 };
